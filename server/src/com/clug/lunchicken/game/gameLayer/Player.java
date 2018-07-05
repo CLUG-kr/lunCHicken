ab37384a27a876388438ad2e32a0347bb503e45f
Mingyu Jo <owlsogul@naver.com>
2018-07-05 22:44:23 +0900

    플레이어 객체 생성

---
+++ server/src/com/clug/lunchicken/game/gameLayer/Player.java
@@ -0,0 +1,37 @@
+package com.clug.lunchicken.game.gameLayer;
+
+import com.clug.lunchicken.game.Client;
+import com.clug.lunchicken.game.gameLayer.gameHandler.Game;
+
+public class Player {
+
+	private Client client;
+	// account
+	private String accountId;
+	
+	// game room
+	private int joinedGameId;
+	private Game joinedGame;
+	
+	// game
+	private int health;
+	private Location location;
+	
+	public void joinGame(Game game) {
+		setJoinedGame(game);
+		setJoinedGameId(game.getGameId());
+	}
+	public int getJoinedGameId() {
+		return joinedGameId;
+	}
+	public void setJoinedGameId(int joinedGameId) {
+		this.joinedGameId = joinedGameId;
+	}
+	public Game getJoinedGame() {
+		return joinedGame;
+	}
+	public void setJoinedGame(Game joinedGame) {
+		this.joinedGame = joinedGame;
+	}
+	
+}
