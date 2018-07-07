459e5ba6308923bd4b73002a01abd64014871719
owlsogul <owlsogul@naver.com>
2018-07-07 16:31:15 +0900

    단계 비교를 쉽게 하기 위해 int 형 level 추가

--- server/src/com/clug/lunchicken/game/gameLayer/gameHandler/GameStatus.java
+++ server/src/com/clug/lunchicken/game/gameLayer/gameHandler/GameStatus.java
@@ -6,26 +6,34 @@ public enum GameStatus {
 	 * 1단계
 	 * 게임이 대기실에 있는 상태
 	 */
-	GAME_READYROOM,
+	GAME_READYROOM(0),
 	/**
 	 * 2단계
 	 * 게임이 시작되는 중
 	 */
-	GAME_START,
+	GAME_START(1),
 	/**
 	 * 3단계
 	 * 게임이 진행 중
 	 */
-	GAME_PLAYING,
+	GAME_PLAYING(2),
 	/**
 	 * 4단계
 	 * 게임이 끝남
 	 */
-	GAME_FINISH,
+	GAME_FINISH(3),
 	/**
 	 * 5단계
 	 * 게임이 없어지는 중
 	 */
-	GAME_DEAD,
+	GAME_DEAD(4);
 	
+	private int level;
+	GameStatus(int level) {
+		this.level = level;
+	}
+	
+	int getLevel() {
+		return this.level;
+	}
 }
