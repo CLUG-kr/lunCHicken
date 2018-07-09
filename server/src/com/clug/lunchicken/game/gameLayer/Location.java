bc5963d7694808c807ff6f074f1497120ed5085c
owlsogul <owlsogul@naver.com>
2018-07-10 05:20:58 +0900

    거리 구하는거 추가

--- server/src/com/clug/lunchicken/game/gameLayer/Location.java
+++ server/src/com/clug/lunchicken/game/gameLayer/Location.java
@@ -24,6 +24,15 @@ public class Location {
 	public void setPosY(double posY) {
 		this.posY = posY;
 	}
-	
+	/**
+	 * 해당 위치와의 거리를 구하는 메소드
+	 * @param loc 거리를 구할 위치
+	 * @return 파라메터가 null 이면 -1, 아닐 경우 거리
+	 */
+	public double getDistance(Location loc) {
+		// null check
+		if (loc == null) return -1;
+		return Math.sqrt(Math.pow(this.getPosX() - loc.getPosX(), 2) + Math.pow(this.getPosY() - loc.getPosY(), 2));
+	}
 	
 }
