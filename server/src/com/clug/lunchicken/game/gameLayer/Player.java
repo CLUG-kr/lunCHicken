ac3b1a14fe19322e710b3a7e743d653671036e2e
Mingyu Jo <owlsogul@naver.com>
2018-07-10 18:33:22 +0900

    플레이어 메소드 정리

--- server/src/com/clug/lunchicken/game/gameLayer/Player.java
+++ server/src/com/clug/lunchicken/game/gameLayer/Player.java
@@ -18,6 +18,16 @@ public class Player {
 	private long healthLastUpdate = 0;
 	private Location location;
 	
+	public Player(Client client, String accountId) {
+		this.setClient(client);
+		this.setAccountId(accountId);
+	}
+	
+	public Client getClient() {return client;}
+	public void setClient(Client client) {this.client = client;}
+	public String getAccountId() {return accountId;}
+	public void setAccountId(String accountId) {this.accountId = accountId;}
+	
 	public void leaveGame() {
 		joinedGameId = -1;
 		joinedGame = null;
@@ -26,39 +36,21 @@ public class Player {
 		setJoinedGame(game);
 		setJoinedGameId(game.getGameId());
 	}
-	public int getJoinedGameId() {
-		return joinedGameId;
-	}
-	public void setJoinedGameId(int joinedGameId) {
-		this.joinedGameId = joinedGameId;
-	}
-	public Game getJoinedGame() {
-		return joinedGame;
-	}
-	public void setJoinedGame(Game joinedGame) {
-		this.joinedGame = joinedGame;
-	}
-	public int getHealth() {
-		return health;
-	}
-	public void setHealth(int health) {
-		this.health = health;
-	}
-	public void addHealth(int health) {
-		this.health += health;
-	}	
-	public long getHealthLastUpdate() {
-		return healthLastUpdate;
-	}
-	public void setHealthLastUpdate(long healthLastUpdate) {
-		this.healthLastUpdate = healthLastUpdate;
-	}
-	public Location getLocation() {
-		return location;
-	}
-	public void setLocation(Location location) {
-		this.location = location;
-	}
+	
+	public int getJoinedGameId() {return joinedGameId;}
+	public void setJoinedGameId(int joinedGameId) {this.joinedGameId = joinedGameId;}
+	public Game getJoinedGame() {return joinedGame;}
+	public void setJoinedGame(Game joinedGame) {this.joinedGame = joinedGame;}
+	
+	public int getHealth() {return health;}
+	public void setHealth(int health) {this.health = health;}
+	public void addHealth(int health) {this.health += health;}
+	
+	public long getHealthLastUpdate() {return healthLastUpdate;}
+	public void setHealthLastUpdate(long healthLastUpdate) {this.healthLastUpdate = healthLastUpdate;}
+	
+	public Location getLocation() {return location;}
+	public void setLocation(Location location) {this.location = location;}
 
 	
 }
