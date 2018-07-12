3aa1fc6129293d4d37ada6780dad6433e205976e
owlsogul <owlsogul@naver.com>
2018-07-13 08:02:57 +0900

    Player 클래스 sendReadyRoomMsg 구현

--- server/src/com/clug/lunchicken/game/gameLayer/Player.java
+++ server/src/com/clug/lunchicken/game/gameLayer/Player.java
@@ -1,5 +1,8 @@
 package com.clug.lunchicken.game.gameLayer;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import com.clug.lunchicken.game.Client;
 import com.clug.lunchicken.game.gameLayer.gameHandler.Game;
 
@@ -52,8 +55,57 @@ public class Player {
 	public Location getLocation() {return location;}
 	public void setLocation(Location location) {this.location = location;}
 
+	/**
+	 * 레디 룸 상태의 게임의 정보를 전송하는 메소드. 메세지 형식은 다음과 같다.<br>
+	 * Message: {
+	 * 	"action":"ready_room_info",
+	 * 	"data":{
+	 * 		"game_id":"(gameId)",
+	 * 		"game_name":"(gameName)",
+	 * 		"current_player":"(currentPlayer)",
+	 * 		"max_player":"(maxPlayer)",
+	 * 		"players":{
+	 * 			{
+	 * 				"accountId":"(accountId)",
+	 * 				"isHost":"(true or false)"
+	 * 			},
+	 * 			{
+	 * 				"accountId":"(accountId)",
+	 * 				"isHost":"(true or false)"
+	 * 			}, 
+	 * 			...
+	 * 		}
+	 * 	}
+	 * }
+	 */
+	@SuppressWarnings("unchecked")
 	public void sendReadyRoomMsg() {
 		
+		// 게임에 들어갔는지 여부 확인
+		// 멀티쓰레딩 과정에서 널 포인터 오류가 날 수 있기 때문에
+		if (this.getJoinedGame() == null) return;
+		Game game = this.getJoinedGame();
+		
+		// JSON parsing
+		JSONObject resObj = new JSONObject();
+		JSONObject resData = new JSONObject();
+		resObj.put("action", "ready_room_info");
+		resData.put("game_id", game.getGameId());
+		resData.put("game_name", game.getGameName());
+		resData.put("current_player", String.valueOf(game.getCurrentPlayer()));
+		resData.put("max_player", String.valueOf(game.getMaxPlayer()));
+		JSONArray playerArr = new JSONArray();
+		for (Player p : game.getAllPlayers()) {
+			JSONObject playerObj = new JSONObject();
+			playerObj.put("accountId", p.getAccountId());
+			playerObj.put("isHost", String.valueOf(game.getHostPlayer() == p));
+			playerArr.add(playerObj);
+		}
+		resData.put("players", playerArr.toJSONString());
+		resObj.put("data", resData.toJSONString());
+		
+		// send
+		client.send(resObj.toJSONString());
 	}
 	
 	public void sendGameStartMsg() {
