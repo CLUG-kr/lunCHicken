eb71e82f97539260db90e0c1235fdc8c723db754
Mingyu Jo <owlsogul@naver.com>
2018-07-12 09:06:16 +0900

    나갔을 때 대기실 변경 사항 알림

--- server/src/com/clug/lunchicken/game/message/MsgLeaveGame.java
+++ server/src/com/clug/lunchicken/game/message/MsgLeaveGame.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONObject;
 import com.clug.lunchicken.game.Client;
 import com.clug.lunchicken.game.GameServer;
 import com.clug.lunchicken.game.gameLayer.Player;
+import com.clug.lunchicken.game.gameLayer.gameHandler.Game;
+import com.clug.lunchicken.game.gameLayer.gameHandler.GameStatus;
 /**
  * request:
  * {
@@ -28,7 +30,17 @@ public class MsgLeaveGame extends Message {
 		Player player = gameServer.getGameHandler().getPlayer(client);
 		if (player == null) return null;
 		if (player.getJoinedGame() == null) return null;
+
+		Game game = player.getJoinedGame();
 		gameServer.getGameHandler().leaveGame(player);
+		
+		// 게임이 대기실이라면 모든 사람들에게 대기실 변경 사항을 보냄
+		if (game.getGameStatus() == GameStatus.GAME_READYROOM) {
+			for (Player p : game.getAllPlayers()) {
+				p.sendReadyRoomMsg();
+			}	
+		}
+		
 		return MessageHandler.NO_RESPONSE;
 	}
 
