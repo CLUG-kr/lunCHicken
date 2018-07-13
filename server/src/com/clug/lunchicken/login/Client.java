19bf4e12cb1f5be28f751038655dc6eb201cd1b3
owlsogul <owlsogul@naver.com>
2018-07-14 04:48:54 +0900

    utf-8 설정

--- server/src/com/clug/lunchicken/login/Client.java
+++ server/src/com/clug/lunchicken/login/Client.java
@@ -3,6 +3,7 @@ package com.clug.lunchicken.login;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
@@ -18,8 +19,8 @@ public class Client implements Runnable{
 		this.handler = handler;
 		this.clientSocket = clientSocket;
 		this.clientThraed = new Thread(this);
-		this.reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		this.writer = new PrintWriter(clientSocket.getOutputStream());
+		this.reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+		this.writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF_8"), true);
 		this.clientThraed.start();
 	}
 	
