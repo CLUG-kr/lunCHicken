d858f37329823248c377f8f7e80e843cdc01fb26
simba328 <simba328@naver.com>
2018-07-17 18:17:46 +0900

    RPI: get raspberrypi ip | server socket

---
+++ imageServer.py
@@ -0,0 +1,18 @@
+﻿## server.py
+
+import socket
+
+def run_server(port=19191):
+	host = ''
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((host, port))	
+	s.listen(1)
+	conn, addr = s.accept()
+
+	while True	
+		conn.sendall(msg)
+	
+	conn.close()
+
+if __name__ == '__main__':
+	run_server()
