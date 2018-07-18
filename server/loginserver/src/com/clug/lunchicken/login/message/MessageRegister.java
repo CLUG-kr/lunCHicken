85039f6d7ba4422d07886f699665b6df08b9d1a3
owlsogul <owlsogul@naver.com>
2018-07-18 19:09:57 +0900

    주석 수정

--- server/loginserver/src/com/clug/lunchicken/login/message/MessageRegister.java
+++ server/loginserver/src/com/clug/lunchicken/login/message/MessageRegister.java
@@ -45,7 +45,6 @@ public class MessageRegister extends Message{
 	 * 회원가입 메세지를 처리하는 메소드
 	 * @param data JSONObject
 	 * @return
-	 * SUCCESS_TOKEN_PARSE(400) - 토큰 분석에 성공했을 경우<br>
 	 * ERR_NOT_VERIFIED(401) - 이메일 인증이 안되어있을 경우<br>
 	 * ERR_WRONG_TOKEN(402) - 잘 못 구성된 토큰일 경우<br>
 	 * 
